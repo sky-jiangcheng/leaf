8141554e9cc728f6c76900fae94e0d55aef4e59a
jiangcheng <jiangcheng05@sinochem.com>
2020-01-16 17:47:37 +0800

    通过反射实现 mvc

---
+++ src/main/java/com/slimframework/mvc/annotation/RequestMapping.java
@@ -0,0 +1,20 @@
+package com.slimframework.mvc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequestMapping {
+    /**
+     * 请求路径
+     */
+    String value() default "";
+
+    /**
+     * 请求方法
+     */
+    RequestMethod method() default RequestMethod.GET;
+}
